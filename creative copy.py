fff16942b1c54b1074aadf28e457289a586a8e2a
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-08-04 21:32:39 -0700

    Update creative copy.py

--- creative copy.py
+++ creative copy.py
@@ -1,4 +1,16 @@
 import nbtlib
+from nbtlib.tag import Int, Byte
+import shutil
 
-file = nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat")
-print(file["Data"]["Player"]["playerGameType"])
+def creativeCopy(worldFolder,newLocation):
+    
+
+    with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat") as file:
+        file["Data"]["Player"]["playerGameType"] = Int(1)
+        file["Data"]["GameType"] = Int(1)
+        file["Data"]["allowCommands"] = Byte(1)
+
+    with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat_old") as file2:
+        file2["Data"]["Player"]["playerGameType"] = Int(1)
+        file2["Data"]["GameType"] = Int(1)
+        file2["Data"]["allowCommands"] = Byte(1)
