590a1fec20a5c93de20342ddc3f5b16782337b7b
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-08-05 14:19:49 -0700

    Update creative copy.py

--- creative copy.py
+++ creative copy.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from time import time
 import nbtlib
 from nbtlib.tag import Int, Byte, String
@@ -6,7 +7,16 @@ from pathlib import Path
 from datetime import datetime
 
 def backup(worldFolder, backupFolder):
-    """Backs up a minecraft world to another folder. 'worldFolder' should be the folder containing the world, and 'backupfolder"""
+    """
+    Backs up a minecraft world to another folder
+    
+    Parameters
+    ---
+    worldFolder: a string or Path object
+        The folder containing the world to back up
+    backupFolder: a string or Path object
+        The folder to which you want the backup to be saved. Note that this function creates a folder containing the world folder
+    """
     
     # Initialize path objects
     worldFolder = Path(worldFolder)
@@ -14,19 +24,39 @@ def backup(worldFolder, backupFolder):
     # get the current time and format it
     fTime = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
     print(worldFolder,backupFolder/fTime)
-    # Copy the folder
-    shutil.copytree(worldFolder,backupFolder/fTime)
+    # Copy the world folder,adding the original game folder back
+    shutil.copytree(worldFolder,backupFolder/fTime/worldFolder.stem)
 
 def creativeCopy(worldFolder, newLocation):
+    """
+    Creates a copy of a world in creative mode and cheats enabled
+    
+    Parameters
+    ---
+    worldFolder: a string or Path object
+        The folder containing the world to back up
+    newLocation: a string or Path object
+        The folder where the world will be copied to.
+    """
+    
+    # Initialize path objects
+    worldFolder = Path(worldFolder)
+    newLocation = Path(newLocation)/(worldFolder.stem+"- Creative Copy") # create the new fold with the smae name as the old, appending " - Creative Copy"
+    # Copy the world folder
+    shutil.copytree(worldFolder,newLocation)
+    
 
-#with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat") as file:
-#    file["Data"]["Player"]["playerGameType"] = Int(1)
-#    file["Data"]["GameType"] = Int(1)
-#    file["Data"]["allowCommands"] = Byte(1)
-#
-#with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat_old") as file2:
-#    file2["Data"]["Player"]["playerGameType"] = Int(1)
-#    file2["Data"]["GameType"] = Int(1)
-#    file2["Data"]["allowCommands"] = Byte(1)
-
-backup(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World",r"C:\Users\TAK\Desktop\Backup\MC")
+    with nbtlib.load(newLocation/"level.dat") as file:
+        file["Data"]["Player"]["playerGameType"] = Int(1)
+        file["Data"]["GameType"] = Int(1)
+        file["Data"]["allowCommands"] = Byte(1)
+        file["Data"]["LevelName"] = String(file["Data"]["LevelName"] + " - Creative Copy")
+    
+    with nbtlib.load(newLocation/"level.dat_old") as file:
+        file["Data"]["Player"]["playerGameType"] = Int(1)
+        file["Data"]["GameType"] = Int(1)
+        file["Data"]["allowCommands"] = Byte(1)
+        file["Data"]["LevelName"] = String(file["Data"]["LevelName"] + " - Creative Copy")
+        
+#backup(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World",r"C:\Users\TAK\Desktop\Backup\MC")
+creativeCopy(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World",r"C:\Users\TAK\AppData\Roaming\.minecraft\saves")
