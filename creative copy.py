ad6593ee565e14b9033e6cd23cfe9fc89850e14e
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-08-05 12:39:39 -0700

    Update creative copy.py

--- creative copy.py
+++ creative copy.py
@@ -1,20 +1,30 @@
+from time import time
 import nbtlib
 from nbtlib.tag import Int, Byte, String
 import shutil
 from pathlib import Path
+from datetime import datetime
 
 def backup(worldFolder, backupFolder):
+    # Initialize path objects
     worldFolder = Path(worldFolder)
     backupFolder = Path(backupFolder)
     
-    shutil.copytree(worldFolder,backupFolder/)
+    # get the current time and format it
+    fTime = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    
+    
+    
+    shutil.copytree(worldFolder,backupFolder/fTime)
 
-with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat") as file:
-    file["Data"]["Player"]["playerGameType"] = Int(1)
-    file["Data"]["GameType"] = Int(1)
-    file["Data"]["allowCommands"] = Byte(1)
 
-with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat_old") as file2:
-    file2["Data"]["Player"]["playerGameType"] = Int(1)
-    file2["Data"]["GameType"] = Int(1)
-    file2["Data"]["allowCommands"] = Byte(1)
+
+#with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat") as file:
+#    file["Data"]["Player"]["playerGameType"] = Int(1)
+#    file["Data"]["GameType"] = Int(1)
+#    file["Data"]["allowCommands"] = Byte(1)
+#
+#with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat_old") as file2:
+#    file2["Data"]["Player"]["playerGameType"] = Int(1)
+#    file2["Data"]["GameType"] = Int(1)
+#    file2["Data"]["allowCommands"] = Byte(1)
