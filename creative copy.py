845fbef43d154d8503054a171333f0de23811c37
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-08-05 11:34:40 -0700

    Update creative copy.py

--- creative copy.py
+++ creative copy.py
@@ -1,18 +1,20 @@
 import nbtlib
-from nbtlib.tag import Int, Byte
+from nbtlib.tag import Int, Byte, String
 import shutil
+from pathlib import Path
 
-def creativeCopy(worldFolder,newLocation):
+def backup(worldFolder, backupFolder):
+    worldFolder = Path(worldFolder)
+    backupFolder = Path(backupFolder)
     
-    
+    shutil.copytree(worldFolder,backupFolder/)
 
+with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat") as file:
+    file["Data"]["Player"]["playerGameType"] = Int(1)
+    file["Data"]["GameType"] = Int(1)
+    file["Data"]["allowCommands"] = Byte(1)
 
-    with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat") as file:
-        file["Data"]["Player"]["playerGameType"] = Int(1)
-        file["Data"]["GameType"] = Int(1)
-        file["Data"]["allowCommands"] = Byte(1)
-
-    with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat_old") as file2:
-        file2["Data"]["Player"]["playerGameType"] = Int(1)
-        file2["Data"]["GameType"] = Int(1)
-        file2["Data"]["allowCommands"] = Byte(1)
+with nbtlib.load(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World\level.dat_old") as file2:
+    file2["Data"]["Player"]["playerGameType"] = Int(1)
+    file2["Data"]["GameType"] = Int(1)
+    file2["Data"]["allowCommands"] = Byte(1)
