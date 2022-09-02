bb455b5ed435ca201ab7834222f65590a746cfd5
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-09-02 15:31:31 -0400

    Update creative copy.py

--- creative copy.py
+++ creative copy.py
@@ -58,5 +58,5 @@ def creativeCopy(worldFolder, newLocation):
         file["Data"]["allowCommands"] = Byte(1)
         file["Data"]["LevelName"] = String(file["Data"]["LevelName"] + " - Creative Copy")
         
-backup(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World",r"C:\Users\TAK\Desktop\Backup\MC")
+backup(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World",r"C:\Users\TAK\OneDrive - purdue.edu\MC Backups")
 #creativeCopy(r"C:\Users\TAK\AppData\Roaming\.minecraft\saves\Good World",r"C:\Users\TAK\AppData\Roaming\.minecraft\saves")
